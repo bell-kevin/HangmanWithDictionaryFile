c0627a4ca917b2be39ddabfdc46b49f4239f1ca8
Kevin Bell <bellKevin@pm.me>
2022-09-20 10:26:38 -0600

    Update HangmanWithDictionaryFile.java

--- hangmanWithDictionaryFile/src/hangmanwithdictionaryfile/HangmanWithDictionaryFile.java
+++ hangmanWithDictionaryFile/src/hangmanwithdictionaryfile/HangmanWithDictionaryFile.java
@@ -6,34 +6,43 @@
  **************************************************************************** */
 package hangmanwithdictionaryfile;
 
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
+import java.nio.file.*;
 
 public class HangmanWithDictionaryFile {
 
     public static void main(String[] args) {
+        String filename = "words.txt";
+        Path path;
         System.out.println("Ch 9 Hangman by Kevin Bell\n");
-        File outputFile = new File("words.txt");
-        FileWriter out;
-        BufferedWriter writeFile;
+        path = Paths.get(filename);
         Scanner computerKeyboardInput = new Scanner(System.in);
-        String word;
-        try {
-            out = new FileWriter(outputFile);
-            writeFile = new BufferedWriter(out);
-            for (int i = 0; i < 6; i++) {
-                System.out.print("Enter new word: ");
-                word = computerKeyboardInput.nextLine();
-                writeFile.write(word);
-                writeFile.newLine();
-            } // end for loop
-            writeFile.close();
-            out.close();
-            System.out.println("Data written to file");
-        } catch (IOException e) {
-            System.out.println("Problem writing to file.");
-            System.out.println("Exception: " + e.getMessage());
-        } // end try/catch block
+        if (Files.exists(path)) {
+            System.out.println("Sorry, that file alreay exists.");
+        } else {
+            File outputFile = new File("words.txt");
+            FileWriter out;
+            BufferedWriter writeFile;
+
+            String word;
+            try {
+                out = new FileWriter(outputFile);
+                writeFile = new BufferedWriter(out);
+                for (int i = 0; i < 6; i++) {
+                    System.out.print("Enter new word: ");
+                    word = computerKeyboardInput.nextLine();
+                    writeFile.write(word);
+                    writeFile.newLine();
+                } // end for loop
+                writeFile.close();
+                out.close();
+                System.out.println("Data written to file");
+            } catch (IOException e) {
+                System.out.println("Problem writing to file.");
+                System.out.println("Exception: " + e.getMessage());
+            } // end try/catch block
+        } // end if/else condition
         Game game = new Game("testing"); //create a new game
         String input;
         char guess;
