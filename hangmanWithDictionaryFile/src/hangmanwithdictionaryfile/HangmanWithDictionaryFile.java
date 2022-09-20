3b92f8f3d7181caecc5ae4538a060297c3065787
Kevin Bell <bellKevin@pm.me>
2022-09-20 10:08:54 -0600

    Update HangmanWithDictionaryFile.java

--- hangmanWithDictionaryFile/src/hangmanwithdictionaryfile/HangmanWithDictionaryFile.java
+++ hangmanWithDictionaryFile/src/hangmanwithdictionaryfile/HangmanWithDictionaryFile.java
@@ -7,16 +7,33 @@
 package hangmanwithdictionaryfile;
 
 import java.util.Scanner;
+import java.io.*;
 
 public class HangmanWithDictionaryFile {
 
     public static void main(String[] args) {
         System.out.println("Ch 9 Hangman by Kevin Bell\n");
+        File outputFile = new File("words.txt");
+        FileWriter out;
+        BufferedWriter writeFile;
         Scanner computerKeyboardInput = new Scanner(System.in);
-        for (int i = 0; i < 6; i++) {
-            System.out.print("Enter new word: ");
-            String word = computerKeyboardInput.nextLine();
-        } // end for loop
+        String word;
+        try {
+            out = new FileWriter(outputFile);
+            writeFile = new BufferedWriter(out);
+            for (int i = 0; i < 6; i++) {
+                System.out.print("Enter new word: ");
+                word = computerKeyboardInput.nextLine();
+                writeFile.write(word);
+                writeFile.newLine();
+            } // end for loop
+            writeFile.close();
+            out.close();
+            System.out.println("Data written to file");
+        } catch (IOException e) {
+            System.out.println("Problem writing to file.");
+            System.out.println("Exception: " + e.getMessage());
+        } // end try/catch block
         Game game = new Game("testing"); //create a new game
         String input;
         char guess;
